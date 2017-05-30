abc7ecc2f161e3a71d62adebfac0ed5d96bc9ad7
Tamas Kenez <tamaskenez@tamaskenez-macbookpro.roam.corp.google.com>
2017-05-30 23:15:41 +0200

    add cat_ul.sh

---
+++ cat_ul.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+cat \
+	ul/type_traits.h \
+	ul/alg_bitset.h \
+	ul/alg_cont_eq_fun.h \
+	ul/alg_elementwise.h \
+	ul/alg_scalar_eq_fun.h \
+	ul/inlinevector.h \
+	ul/to_string.h \
+	ul/ul.h
+
