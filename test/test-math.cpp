15a3218d09c3ad3802dec4196e86e419ff89f618
Tamas Kenez <tamas.kenez@gmail.com>
2017-10-28 20:34:54 +0200

    Add math/sign.

--- test/test-math.cpp
+++ test/test-math.cpp
@@ -1,9 +1,11 @@
 #undef NDEBUG
 
 #include <cassert>
+#include <climits>
 
 #include "ul/math.h"
 
+using ul::sign;
 using ul::within_co;
 
 int main()
@@ -14,5 +16,23 @@ int main()
     assert(within_co(9, 0, 10));
     assert(!within_co(10, 0, 10));
     assert(!within_co(11, 0, 10));
+
+    assert(sign(INT_MIN) == -1);
+    assert(sign(-2) == -1);
+    assert(sign(-1) == -1);
+    assert(sign(0) == 0);
+    assert(sign(1) == 1);
+    assert(sign(2) == 1);
+    assert(sign(INT_MAX) == 1);
+    assert(sign(UINT_MAX) == 1);
+    assert(sign(-INFINITY) == -1);
+    assert(sign(-2.3456789) == -1);
+    assert(sign(-1.0) == -1);
+    assert(sign(-1e-200) == -1);
+    assert(sign(0) == 0);
+    assert(sign(1e-200) == 1);
+    assert(sign(1.0) == 1);
+    assert(sign(2.3456789) == 1);
+    assert(sign(INFINITY) == 1);
     return 0;
 }
--- ul/math.h
+++ ul/math.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <cmath>
 
 namespace ul {
 template <class X, class L, class U>
@@ -9,4 +10,11 @@ bool within_co(const X& x, const L& lower, const U& upper)
     assert(lower <= upper);
     return lower <= x && x < upper;
 }
+
+template <class T>
+constexpr int sign(T x) noexcept
+{
+    return x == T(0) ? 0 : (std::signbit(x) ? -1 : 1);
+}
+
 }  // namespace ul
